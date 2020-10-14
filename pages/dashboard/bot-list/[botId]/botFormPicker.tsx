239aed6da291e24478d882b8179f2775fb9cfb77
kacluk123 <sakuy124@gmail.com>
2020-10-14 22:20:50 +0200

    dd

---
+++ pages/dashboard/bot-list/[botId]/botFormPicker.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { botTypes, IUIResponseAdBotConfig, IUIResponseBot } from '../../../../services/api/bots/bots.types'
+
+interface BotFormPicker {
+  bot: IUIResponseBot
+}
+
+const BotFormPicker: React.FC<BotFormPicker> = ({ bot }) => {
+  
+}
