afaac5f6fd8fdba62d2d7a75954891b446e09349
kacluk123 <sakuy124@gmail.com>
2021-04-11 16:34:34 +0200

    dd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,12 @@ import useSWR, { SWRConfig } from 'swr'
 import { GlobalStyle } from '../styles/styledComponentsGlobal'
 import cogoToast from 'cogo-toast';
 
+const cli = require('next/dist/cli/next-start');
+
+cli.nextStart([
+  '-p', process.env.PORT || 80,
+]);
+
 function MyApp({ Component, pageProps }) {
   return (
     <SWRConfig value={{
